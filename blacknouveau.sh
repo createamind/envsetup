5ffc7257c0b1893b71d1e1c06c120cb00011a9a5
zdx <zdx@zdxdeMacBook-Pro.local>
2020-01-14 16:14:31 +0800

    03

--- blacknouveau.sh
+++ blacknouveau.sh
@@ -1,9 +1,9 @@
 ###
 
 
-#wget cuda
+#wget cuda     10  1604
 
-
+wget https://developer.nvidia.com/compute/cuda/10.0/Prod/local_installers/cuda_10.0.130_410.48_linux
 
 
 
@@ -18,3 +18,5 @@ cat /etc/modprobe.d/blacklist-nouveau.conf
 sudo update-initramfs -u
 
 sudo reboot
+
+sudo /etc/init.d/lightdm stop
--- envsetup.sh
+++ envsetup.sh
@@ -1,7 +1,28 @@
 #
 #
+
+
+#https://askubuntu.com/questions/425754/how-do-i-run-a-sudo-command-inside-a-script/425990
+
+
+
 sudo apt update
 sudo apt install ssh git htop vim tmux curl subversion zsh
+
+
+#install python 37
+sudo apt install software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update
+sudo apt install python3.7
+
+
+#python venv
+sudo apt install python-pip
+sudo /usr/bin/easy_install virtualenv
+pip install virtualenv
+virtualenv -p python3.7 py37venv
+
 
 
 #change pip source
@@ -15,24 +36,9 @@ echo 'trusted-host=mirrors.aliyun.com' >>  ~/.pip/pip.conf
 echo "conform-----------"
 cat   ~/.pip/pip.conf
 
-#install python 37
-sudo apt install software-properties-common
-sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt update
-sudo apt install python3.7
-
-
-
-
-#python venv
-sudo apt install python-pip
-pip install virtualenv
-sudo /usr/bin/easy_install virtualenv
-
-
-
 
 #zsh
 bash ./inzsh.sh
 
 
+#echo "source ~/py37venv/bin/activate" >> .zshrc
