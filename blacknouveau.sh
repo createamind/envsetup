31cc2f9f56fdef43585867981ace9bcb9cf30b0c
zdx <zdx@zdxdeMacBook-Pro.local>
2020-04-03 17:06:32 +0800

    ++

--- blacknouveau.sh
+++ blacknouveau.sh
@@ -3,6 +3,7 @@
 
 #wget cuda     10  1604
 
+##cuda version for tensorflow!!
 wget https://developer.nvidia.com/compute/cuda/10.0/Prod/local_installers/cuda_10.0.130_410.48_linux
 
 
@@ -20,3 +21,9 @@ sudo update-initramfs -u
 sudo reboot
 
 sudo /etc/init.d/lightdm stop
+sudo /etc/init.d/gdm3  stop
+
+sudo apt-get install build-essential
+
+sudo  apt-get install linux-image-$(uname -r)    linux-headers-$(uname -r)
+sudo apt install dkms          ## https://blog.csdn.net/baobei0112/article/details/81488478
--- envsetup.sh
+++ envsetup.sh
@@ -2,30 +2,25 @@
 #
 
 #https://askubuntu.com/questions/425754/how-do-i-run-a-sudo-command-inside-a-script/425990
-
+##tmux cache buffer size  https://stackoverflow.com/questions/18760281/how-to-increase-scrollback-buffer-size-in-tmux
 #https://github.com/createamind/Planet/issues/3
 #https://linuxize.com/post/how-to-create-a-sudo-user-on-ubuntu/
+# adduser username;;usermod -aG sudo username
 #https://www.runoob.com/w3cnote/set-ssh-login-key.html
+#####https://stackoverflow.com/questions/14547631/python-locale-error-unsupported-locale-setting/36257050
 
 
 sudo apt update
 
-
-sudo apt-get install -y python-numpy  cmake zlib1g-dev libjpeg-dev xvfb  python-pip  python3-venv make golang wget unzip git fceux virtualenv      tmux      ssh git htop vim tmux curl subversion zsh  libopenmpi-dev libxrender1  lrzsz   libtbb2
-
+sudo apt-get install -y python-numpy  cmake zlib1g-dev libjpeg-dev xvfb  python-pip  python3-venv make golang wget unzip git fceux virtualenv   cpufrequtils   tmux      ssh git htop vim tmux curl subversion zsh  libopenmpi-dev libxrender1  lrzsz   libtbb2    python3.6-dev        linux-tools-$(uname -r)      net-tools
 
 
 #install python 37
-echo "-------------------------------------------------------------------------------------------------install python 37"
-sudo apt install software-properties-common     -y
-sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt update
-sudo apt install python3.7 python3.7-dev -y
-
-
-
-
-
+#echo "-------------------------------------------------------------------------------------------------install python 37"
+#sudo apt install software-properties-common     -y
+#sudo add-apt-repository ppa:deadsnakes/ppa
+#sudo apt update
+#sudo apt install python3.7 python3.7-dev -y
 
 
 #change pip source
@@ -41,29 +36,47 @@ echo "conform-----------"
 cat   ~/.pip/pip.conf
 
 
-#####https://stackoverflow.com/questions/14547631/python-locale-error-unsupported-locale-setting/36257050
-
 #pip install virtualenv
 #sudo /usr/bin/easy_install virtualenv
-virtualenv -p python3.7 ~/py37venv
-source ~/py37venv/bin/activate
+virtualenv -p python3.6 ~/py36venv
+source ~/py36venv/bin/activate
 pip install -r ./requirement/req.txt
+pip list
 
-
-
+#DDRL  SQN
+#pip install ray==0.7.6 lz4 psutil requests setproctitle
 
 #zsh
 bash ./inzsh.sh
 
-## bashrc add zsh??
+#svn checkout
+# env maybe use py36 more stable;;
+#git clone code         ; pip install -e .
+             ##cp env to code dir
 
-#echo " " >> ~/.zshrc:q
-#echo "source ~/py37venv/bin/activate" >> ~/.zshrc
+
+#gcc version??     540
+#sudo apt-get install gcc-5
+#gcc -v
+#cat /etc/issue
+#cd /usr/bin
+#sudo rm gcc
+#sudo ln -s  gcc-5 gcc
+
+
+## bashrc add zsh??
+#echo " " >> ~/.zshrc
+#echo "source ~/py36venv/bin/activate" >> ~/.zshrc
 #echo " " >> ~/.zshrc
 
+##add zsh??
 
-#svn checkout
-#git clone code         ; pip install -e .
-#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/
+#source ~/py36venv/bin/activate
+#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/jz/rl_game/game
+#cd /home/jz/zdx/github/game_sample/stable_baselines/game
 
-##tmux cache buffer size  https://stackoverflow.com/questions/18760281/how-to-increase-scrollback-buffer-size-in-tmux
+## code pull or same make version ok?
+
+##for p in `ls /proc/4989/task`; do "/usr/bin/taskset  ‐c '15-30'  -p $p" ; done
+
+##sudo cpupower -c all frequency-set -g performance    ; cat /proc/cpuinfo |grep MHz|uniq
